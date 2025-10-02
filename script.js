27cab07be7cad7a69d638a09986480931c0dbc03
knightbus3 <quitoan0712@gmail.com>
2025-10-02 16:31:21 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,36 @@ const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
 
 // タスク追加ボタンのクリック処理
-addBtn.addEventListener('click', () => {
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText === '') return;
+
+    // 新しい<li>要素を作る
+    const li = document.createElement('li');
+    li.textContent = taskText;
+
+    // 削除ボタンを作成
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = '消去';
+    deleteBtn.addEventListener('click', () => {
+        li.remove();
+    });
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    // 入力欄を空にする
+    taskInput.value = '';
+}
+/*addBtn.addEventListener('click', () => {
   const taskText = taskInput.value.trim();
   if (taskText === '') return;
 
+  // Enterキーでもタスク追加
+  taskInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        addBtn.click();
+    }
+    });
   // 新しい<li>要素を作る
   const li = document.createElement('li');
   li.textContent = taskText;
@@ -24,4 +50,14 @@ addBtn.addEventListener('click', () => {
 
   // 入力欄を空にする
   taskInput.value = '';
+});
+*/
+
+//ボタンでタスク追加
+addBtn.addEventListener('click', addTask);
+// Enterキーでもタスク追加
+taskInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    addTask();
+  }
 });
